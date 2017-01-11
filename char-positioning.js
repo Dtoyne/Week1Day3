0cdefac8063930373a686acc1f9abadcd39e80a1
Dylan <dylantoyne@gmail.com>
2017-01-11 16:56:51 +0000

    Added a for-in loop in an attempt to break away from dependence on traditional for loops.

--- char-positioning.js
+++ char-positioning.js
@@ -1,6 +1,8 @@
 // When passing arguments through the command line, be sure to delimit the input with quotes:
 // This will allow the arguments (however many) to be passed through the program as one argument.
 
+// With a for loop:
+
 var str = process.argv.slice(2).toString()
 
 function countLetters (str) {
@@ -15,5 +17,24 @@ function countLetters (str) {
       }
     return object;
   }
+
+console.log(countLetters(str));
+
+// With a for-in loop:
+
+var str = process.argv.slice(2).toString()
+
+function countLetters(str) {
+  var uniqueLetters = "";
+  var object = {};
+    for (var i in str) {
+      if (object.hasOwnProperty(str[i])) {
+        object[str[i]].push(i);
+      } else {
+        object[str[i]] = [i]
+      }
+    }
+  return object;
+}
 
 console.log(countLetters(str));
